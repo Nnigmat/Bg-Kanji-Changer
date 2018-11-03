ccdd737de59527f2ca39e0a53d355763e898bab8
Nnigmat <nnigmat@gmail.com>
2018-11-03 19:35:56 +0300

    Almost final

--- kanji_bg.py
+++ kanji_bg.py
@@ -7,22 +7,22 @@ def draw(kanji, path_bg='src/wallpaper.png', path_font='', font_color=(239, 240,
     draw = ImageDraw.Draw(image)
 
     # Position and text of kanji
-    position = (3000, 1700)
+    position = (2900, 1900)
     text = kanji[0]
-    font = ImageFont.truetype(font=path_font, size=650)
+    font = ImageFont.truetype(font=path_font, size=600)
     draw.text(xy=position, text=text, fill=font_color, font=font)
 
     if kanji[0] != kanji[1]:
         # Position and text of furigana
-        position = (3100, 2500)
+        position = (3000, 2600)
         text = kanji[1]
         font = ImageFont.truetype(font=path_font, size=200)
         draw.text(xy=position, text=text, fill=font_color, font=font)
 
     # Position and text of meaning
-    position = (3200, 2800)
+    position = (3100, 2900)
     text = kanji[3]
-    font = ImageFont.truetype(font=path_font, size=150)
+    font = ImageFont.truetype(font=path_font, size=100)
     draw.text(xy=position, text=text, fill=font_color, font=font)
 
 
